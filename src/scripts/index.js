3823bbda7e1bf9e34ad0a2f4de15456ae52e88c2
Nanda Safiq Alfiansyah <nandasafiqalfiansyah@gmail.com>
2025-04-18 01:26:39 +0700

    [fix] api servide

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-        .register('/sw.js', { type: 'module' })
+        .register('sw.js')
         .then((registration) => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
--- webpack.prod.js
+++ webpack.prod.js
@@ -31,7 +31,7 @@ module.exports = merge(common, {
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
     new InjectManifest({
-      swSrc: path.resolve(__dirname, '/src/public/sw.js'),
+      swSrc: path.resolve(__dirname, 'src/public/sw.js'),
       swDest: 'sw.bundle.js',
     }),
   ],
